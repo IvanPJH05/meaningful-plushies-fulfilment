213ed225b0db979d46da01e67b91e71459349231
IvanPJH05 <ivanphang05@gmail.com>
2026-07-31 13:56:08 +0800

    Label emojis in chat exports

--- app/api/crm/inbox/export/route.ts
+++ app/api/crm/inbox/export/route.ts
@@ -17,7 +17,7 @@ function printableText(value: string | null | undefined) {
     .replace(/[\u2018\u2019]/g, "'")
     .replace(/[\u201C\u201D]/g, '"')
     .replace(/[\u2013\u2014]/g, "-")
-    .replace(/[^\x20-\x7E\n\r\t]/g, "?")
+    .replace(/[^\x20-\x7E\n\r\t]+/g, "[emoji]")
     .replace(/\r\n/g, "\n");
 }
 
